809c4749ebe5fe734631f946ea4c6dac4f6909de
Diegou2203 <diegouribe990@gmail.com>
2026-02-26 11:03:02 -0500

    commits inicial

---
+++ proyecto_pokedex/src/index.js
@@ -0,0 +1,18 @@
+import ReactDOM from 'react-dom/client';
+import App from './components/App.js';
+import React from 'react';
+import { QueryClient } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
+export const queryClient = new QueryClient();
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
+
+
+
